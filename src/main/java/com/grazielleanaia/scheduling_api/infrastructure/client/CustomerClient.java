7abee4bc26d9ae1e9b53f1651f0031f802e6b555
Grazielle <grazielle.anaia@gmail.com>
2025-04-25 14:04:21 -0400

    Correction of the get endpoint

--- src/main/java/com/grazielleanaia/scheduling_api/infrastructure/client/CustomerClient.java
+++ src/main/java/com/grazielleanaia/scheduling_api/infrastructure/client/CustomerClient.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 public interface CustomerClient {
 
-    @GetMapping
+    @GetMapping("/customer")
     CustomerDTO findCustomerByEmail(@RequestParam("email") String email,
                                     @RequestHeader("Authorization") String token);
 
