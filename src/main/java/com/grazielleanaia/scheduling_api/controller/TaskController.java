a4b40c302bf225560e360c3988c6c29bed12f72a
Grazielle <grazielle.anaia@gmail.com>
2025-04-29 12:23:54 -0400

    Added method Get and Delete to TaskController

--- src/main/java/com/grazielleanaia/scheduling_api/controller/TaskController.java
+++ src/main/java/com/grazielleanaia/scheduling_api/controller/TaskController.java
@@ -3,8 +3,12 @@ package com.grazielleanaia.scheduling_api.controller;
 import com.grazielleanaia.scheduling_api.business.TaskService;
 import com.grazielleanaia.scheduling_api.business.dto.TaskDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/task")
@@ -20,4 +24,22 @@ public class TaskController {
         return ResponseEntity.ok(taskService.createTask(taskDTO, token));
     }
 
+    @GetMapping
+    public ResponseEntity<List<TaskDTO>> findTaskList(@RequestHeader("Authorization") String token) {
+        return ResponseEntity.ok(taskService.findTaskByEmail(token));
+    }
+
+    @GetMapping("/events")
+    public ResponseEntity<List<TaskDTO>> findTaskByPeriod(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+                                                          LocalDateTime initialDate,
+                                                          @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+                                                          LocalDateTime finalDate) {
+        return ResponseEntity.ok(taskService.findTaskByPeriod(initialDate, finalDate));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteTaskById(@RequestParam("id") String id) {
+        taskService.deleteTaskById(id);
+        return ResponseEntity.ok().build();
+    }
 }
